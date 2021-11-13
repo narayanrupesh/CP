bfc6580ec8ed2784a9a8d870b4a7b103f84bb094
narayanrupesh <93879224+narayanrupesh@users.noreply.github.com>
2021-11-13 15:09:45 +0530

    Added solution to Codeforces Round #244 (Div 2), problem A

---
+++ Codeforces/427a.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int numOfficer = 0, numCrimes = 0;
+    int input;
+
+    while (n--)
+    {
+        cin >> input;
+        numOfficer = ( numOfficer <= 0 ? input : numOfficer + input);
+        numCrimes = ( numOfficer >= 0 ? numCrimes : numCrimes + 1 );
+    }
+
+    cout << numCrimes << "\n";
+
+    return 0;
+}
+
