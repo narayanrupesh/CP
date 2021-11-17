7e6b7b08ad92fe103e1dd3dd61abbf0a65e14675
Rupesh Narayan <93879224+narayanrupesh@users.noreply.github.com>
2021-11-17 23:23:44 +0530

    Added solution to Codeforces Beta Round #47, problem A

---
+++ Codeforces/50a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int m, n;
+    cin >> m >> n;
+
+    cout << ((m*n)/ 2) << "\n";
+
+    return 0;
+}
+
+// Problem link: https://codeforces.com/problemset/problem/50/A
